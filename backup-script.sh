37e765524a49ff12f1217a44e34b6f142062f8cb
= <up730418@backer-upper.c.clocoss-2017.internal>
2018-01-25 13:07:05 +0000

    Beginings of buckets

---
+++ backup-script.sh
@@ -0,0 +1,2 @@
+ gsutil mb -l europe-west2 gs://up730418-bucket
+ gcloud beta datastore export --namespaces='up730418s4' gs://up730418-bucket
---
+++ cron-job.sh
@@ -0,0 +1,1 @@
+gcloud beta datastore export --namespaces='up730418s4' gs://up730418-bucket
