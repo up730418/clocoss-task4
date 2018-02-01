0e0bffdb12c13200ca8528de129a42d1928ed1da
up730418 <up730418@port.ac.uk>
2018-02-01 12:46:14 +0000

    Update backup-script.sh

--- backup-script.sh
+++ backup-script.sh
@@ -1,4 +1,4 @@
  bucketName="up730418-bucket"
  namespaces="up730418s4"
  gsutil mb -l europe-west2 gs://$bucketName
- gcloud beta datastore export --namespaces=&namespaces gs://$bucketName
+ gcloud beta datastore export --namespaces=$namespaces gs://$bucketName
